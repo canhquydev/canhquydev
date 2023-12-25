75a9c08a32a6b2cc8dfb96473e83f9d5e4eab233
canhquydev <canhquydev@hotmail.com>
2023-12-25 12:06:28 +0700

    bai tap ve nha

--- bai tap buoi 4/S2_HoCanhQuy_Bai5.cpp
+++ bai tap buoi 4/S2_HoCanhQuy_Bai5.cpp
@@ -3,6 +3,40 @@ using namespace std;
 
 int main()
 {
-
+    int n, cnt2 = 0, cnt1 = 0, sum = 0;
+    cin >> n;
+    int arr[n];
+    for(int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+        if(arr[i] == 200)
+        {
+            cnt2++;
+        }
+        else if(arr[i] == 100)
+        {
+            cnt1++;
+        }
+        else
+        {
+            i--;
+        }
+    }
+    for(int i = 0; i < n; i++)
+    {
+        sum += arr[i];
+    }
+    if(sum % 2 != 0)
+    {
+        cout << "NO";
+    }
+    if((cnt2 % 2 != 0 && cnt1 % 2 == 0) || (cnt2 % 2 == 0 && cnt1 % 2 == 0))
+    {
+        cout << "YES";
+    }
+    else
+    {
+        cout << "NO";
+    }
     return 0;
 }
